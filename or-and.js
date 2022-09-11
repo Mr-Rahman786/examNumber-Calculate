a00653a022213e07dc7932469f803f1e2a13b100
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-11 09:10:15 +0800

    added price

--- or-and.js
+++ or-and.js
@@ -11,3 +11,18 @@ if (mathExam > 90 && accontingExam > 82 && financeExam > 93 && englishExam > 75)
 else {
     console.log('sorry to say you are faild')
 }
+
+
+// how may it diploy
+
+var needPhone = true;
+var iPhade = 1000;
+var airpod = 500;
+var haveMoney = true;
+
+if (needPhone > false && iPhade > 1020 && airpod > 501 || haveMoney > false) {
+    console.log('congreats sir you are selected by won')
+}
+else {
+    console.log("sorry to say you can't be selected")
+}
